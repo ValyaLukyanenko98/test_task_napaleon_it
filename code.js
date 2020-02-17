f612f518d8175a71db8f1f0c11e5e192351cab13
Valya <valyalukyaneno0@gmail.com>
2020-02-17 21:41:07 +0500

    Inital commit

---
+++ code.js
@@ -0,0 +1,9 @@
+let showNumber = document.getElementById('showNumber');
+
+
+showNumber.onclick = function () {
+    showNumber.textContent = '+7 922 731 80 06';
+    showNumber.style.cursor ="default";
+};
+
+
